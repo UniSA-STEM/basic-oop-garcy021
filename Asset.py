623b3a95bcf3085cc8f80d211388cb916cd89847
Chirag <garcy021@mymail.unisa.edu.au>
2025-10-18 17:20:47 +1030

    class asset

--- Asset.py
+++ Asset.py
@@ -6,3 +6,15 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+from dataclasses import dataclass
+
+@dataclass
+class Asset:
+    name: str
+    description: str
+    encrypted: bool = False
+
+    def __str__(self) -> str:
+        base = f"{self.name}: {self.description}"
+        return f"{base} [Encrypted]" if self.encrypted else base
