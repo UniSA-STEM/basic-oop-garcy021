4d6bd6faafd747ec2fac89ef3aa4c3c86edb6501
Chirag <garcy021@mymail.unisa.edu.au>
2025-10-20 17:12:25 +1030

    fixed some syntax errors and added remaining comments

--- Asset.py
+++ Asset.py
@@ -1,24 +1,22 @@
 """
 File: Asset.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: Defines the Asset class for items that can be stored, encrypted, or transferred between hackers and rigs.
+Author: CHIRAG GARG
+ID: 110395864
+Username: GARCY021
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
+"""
+Represents a digital asset that hackers can own, store or transfer.
+Each asset has a name, a description, and an encrypted state.
+"""
 class Asset:
-    """
-    Represents a digital asset in the cyberpunk world.
-    Each asset has a name, a description, and may be encrypted.
-    """
-
     def __init__(self, name, description, encrypted=False):
         self._name = name
         self._description = description
         self._encrypted = encrypted
 
-    # ----- Properties -----
     @property
     def name(self):
         return self._name
@@ -35,7 +33,7 @@ class Asset:
     def encrypted(self, value):
         self._encrypted = value
 
-    # ----- String Representation -----
+#Returns a readable text version of the asset. If encrypted, '[Encrypted]' appears at the end.
     def __str__(self):
         base = f"{self._name}: {self._description}"
         return f"{base} [Encrypted]" if self._encrypted else base
